cecfe9417c42ebec744675a1d3c4db38a87d3916
Harsh-Trivedi <60310287+Harsh-Trivedi@users.noreply.github.com>
2020-02-27 23:45:59 +0530

    Add Comments

--- dictator.js
+++ dictator.js
@@ -1,19 +1,22 @@
+//Author  : Harsh K Trivdi
+///This function is used to serialize the form to json
+//pass form id to getFormJsonByID function and it will return a Json Object.
 
-	function getFormJsonByID( formID ) {
-		var form = document.getElementById(formID )
-		var obj = {};
-		var elements = form.querySelectorAll( "input, select, textarea" );
-		for( var i = 0; i < elements.length; ++i ) {
-			var element = elements[i];
-			var name = element.name;
-			var value = element.value;
+function getFormJsonByID( formID ) {
+	var form = document.getElementById(formID )
+	var obj = {};
+	var elements = form.querySelectorAll( "input, select, textarea" );
+	for( var i = 0; i < elements.length; ++i ) {
+		var element = elements[i];
+		var name = element.name;
+		var value = element.value;
 
-			if( name ) {
-				obj[ name ] = value;
-			}
+		if( name ) {
+			obj[ name ] = value;
 		}
-
-		return JSON.stringify( obj );
 	}
+
+	return JSON.stringify( obj );
+}
 
 	
