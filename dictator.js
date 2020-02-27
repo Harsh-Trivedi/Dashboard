6c1848a10ab93d935c870e83c2511f122ff41bdb
Harsh-Trivedi <60310287+Harsh-Trivedi@users.noreply.github.com>
2020-02-27 23:42:55 +0530

    Form to JSON
    Function Name  : getFormJsonByID(form)
    parameters : 
              form :  Id of form.
    
    Serialize Form Data Into JSON Object

---
+++ dictator.js
@@ -0,0 +1,19 @@
+
+	function getFormJsonByID( formID ) {
+		var form = document.getElementById(formID )
+		var obj = {};
+		var elements = form.querySelectorAll( "input, select, textarea" );
+		for( var i = 0; i < elements.length; ++i ) {
+			var element = elements[i];
+			var name = element.name;
+			var value = element.value;
+
+			if( name ) {
+				obj[ name ] = value;
+			}
+		}
+
+		return JSON.stringify( obj );
+	}
+
+	
